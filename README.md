8cb118055545d214889f44bfd4ed3151974ab287
lr1ngtz <sko0hlol@gmail.com>
2022-06-27 15:49:29 +0200

    First program

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Programming from the Ground Up by Jonathan Bartlett
+
+
+### Assemble 'as' command
+### Link 'ld' command
---
+++ exit.s
@@ -0,0 +1,34 @@
+# PURPOSE: Simple program that exits and returns a
+# status code back to the Linux kernel
+# ASSEMBLE as exit.s -o exit.o
+# LINK ld exit.o -o exit
+#
+# INPUT: none
+#
+# OUTPUT: returns a status code.
+# This can be viewed by typing echo $?
+# after running the program
+#
+#VARIABLES:
+# %eax holds the system call number
+# %ebx holds the return status
+
+ .section .data
+
+ .section .text
+ .globl _start
+_start:
+# this is the linux kernel command
+# number (system call) for exiting a program
+movl $1, %eax
+
+# this is the status number we will
+# return to the operating system.
+# Change this around and it will
+# return different things to echo $?
+movl $0, %ebx #
+
+# this wakes up the kernel to run
+# the exit command
+int $0x80 
+
